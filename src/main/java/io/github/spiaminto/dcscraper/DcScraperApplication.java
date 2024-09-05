c3a74fbc6c5937e2c9a46529d96f8f6d5c09f008
spiaminto <spiaminto@gmail.com>
2024-09-05 17:52:16 +0900

    init

---
+++ src/main/java/io/github/spiaminto/dcscraper/DcScraperApplication.java
@@ -0,0 +1,13 @@
+package io.github.spiaminto.dcscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcScraperApplication.class, args);
+    }
+
+}
